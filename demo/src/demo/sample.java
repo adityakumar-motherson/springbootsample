7b2a74f4b060b1de29c043553c07ee198538044b
Aditya Kumar <Aditya.Kumar03@motherson.com>
2024-05-09 15:37:54 +0530

    msg1

--- demo/src/demo/sample.java
+++ demo/src/demo/sample.java
@@ -4,7 +4,7 @@ public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sample project4");
+		System.out.println("Sample project41");
 	}
 
 }
